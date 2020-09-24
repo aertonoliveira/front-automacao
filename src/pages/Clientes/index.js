854b5a9196afc083bc59985dae641b21a4c04fb7
Rafael Barbosa <shinzootk@gmail.com>
2020-09-24 00:24:33 -0300

    Adicionado loading e paginação na listagem de clientes

--- src/pages/Clientes/index.js
+++ src/pages/Clientes/index.js
@@ -9,6 +9,7 @@ import { Row, Col, Card, CardTitle, CardBody, Form } from 'reactstrap';
 import Dropdown from '../../components/custom/dropdown';
 
 import { listagemClienteRequest } from '../../store/modules/clientes/actions';
+import Pagination from 'react-js-pagination';
 
 const Clientes = () => {
   // Validação
@@ -23,7 +24,7 @@ const Clientes = () => {
   const resolver = useYupValidationResolver(validationSchema);
   const { handleSubmit, register } = useForm({ resolver });
   const dispatch = useDispatch();
-  const isLoading = useSelector((state) => state.cliente.loading);
+  const loading = useSelector((state) => state.cliente.loading);
   const listagem = useSelector((state) => state.cliente.listagemClientes);
 
   const listagemClientes = (data) => {
@@ -37,6 +38,10 @@ const Clientes = () => {
     };
     dispatch(listagemClienteRequest(data));
   }, [dispatch]);
+
+  const realizarPaginacao = (numeroPagina) => {
+    dispatch(listagemClienteRequest(numeroPagina));
+  };
 
   return (
     <>
@@ -56,69 +61,96 @@ const Clientes = () => {
               </Link>
             </div>
             <Form onSubmit={handleSubmit(listagemClientes)}>
-              <CardBody className="iq-card-body">
-                <Row className="mb-4">
-                  <Col sm="12" md="6" lg="4">
-                    <select className="form-control" name="type" ref={register}>
-                      <option value="Analista Senior">Analista Senior</option>
-                      <option value="Gestor de analista">
-                        Gestor de analista
-                      </option>
-                      <option value="Analista pleno">Analista pleno</option>
-                      <option value="Cliente">Cliente</option>
-                      <option value="Parceiro">Parceiro</option>
-                      <option value="Trader">Trader</option>
-                    </select>
-                  </Col>
+              {loading ? (
+                <img
+                  width="100"
+                  height="100"
+                  src={require('../../assets/images/loading.gif')}
+                  alt="Carregando..."
+                />
+              ) : (
+                <CardBody className="iq-card-body">
+                  <Row className="mb-4">
+                    <Col sm="12" md="6" lg="4">
+                      <select
+                        className="form-control"
+                        name="type"
+                        ref={register}
+                      >
+                        <option value="Analista Senior">Analista Senior</option>
+                        <option value="Gestor de analista">
+                          Gestor de analista
+                        </option>
+                        <option value="Analista pleno">Analista pleno</option>
+                        <option value="Cliente">Cliente</option>
+                        <option value="Parceiro">Parceiro</option>
+                        <option value="Trader">Trader</option>
+                      </select>
+                    </Col>
 
-                  <Col sm="12" md="6" lg="4">
-                    <button
-                      type="submit"
-                      color="primary"
-                      className="btn btn-primary btn-lg"
-                    >
-                      {isLoading ? 'Carregando...' : 'Pesquisar'}
-                    </button>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col sm="12">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th scope="col">ID</th>
-                          <th scope="col">Nome</th>
-                          <th scope="col">CPF</th>
-                          <th scope="col">Email</th>
-                          <th scope="col">Celular</th>
-                          <th scope="col">Status</th>
-                          <th scope="col">Cargo</th>
-                          <th scope="col">Saldo</th>
-                          <th scope="col"></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {listagem &&
-                          listagem.data.map((cliente, index) => (
-                            <tr key={index}>
-                              <th scope="row">{cliente.id}</th>
-                              <td>{cliente.name}</td>
-                              <td>{cliente.cpf}</td>
-                              <td>{cliente.email}</td>
-                              <td>{cliente.celular}</td>
-                              <td>{cliente.ativo}</td>
-                              <td>{cliente.parent.name}</td>
-                              <td>{cliente.saldo_conta.valor}</td>
-                              <td>
-                                <Dropdown clienteId={cliente.id} />
-                              </td>
-                            </tr>
-                          ))}
-                      </tbody>
-                    </table>
-                  </Col>
-                </Row>
-              </CardBody>
+                    <Col sm="12" md="6" lg="4">
+                      <button
+                        type="submit"
+                        color="primary"
+                        className="btn btn-primary btn-lg"
+                      >
+                        {loading ? 'Carregando...' : 'Pesquisar'}
+                      </button>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col sm="12">
+                      <table className="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">CPF</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Celular</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Cargo</th>
+                            <th scope="col">Saldo</th>
+                            <th scope="col"></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {listagem &&
+                            listagem.data.map((cliente, index) => (
+                              <tr key={index}>
+                                <th scope="row">{cliente.id}</th>
+                                <td>{cliente.name}</td>
+                                <td>{cliente.cpf}</td>
+                                <td>{cliente.email}</td>
+                                <td>{cliente.celular}</td>
+                                <td>{cliente.ativo}</td>
+                                <td>{cliente.parent.name}</td>
+                                <td>{cliente.saldo_conta.valor}</td>
+                                <td>
+                                  <Dropdown clienteId={cliente.id} />
+                                </td>
+                              </tr>
+                            ))}
+                        </tbody>
+                      </table>
+                      {listagem && (
+                        <Pagination
+                          activePage={listagem.current_page}
+                          totalItemsCount={listagem.total}
+                          itemsCountPerPage={listagem.per_page}
+                          onChange={(numeroPagina) =>
+                            realizarPaginacao(numeroPagina)
+                          }
+                          itemClass="page-item"
+                          linkClass="page-link"
+                          firstPageText="Início"
+                          lastPageText="Fim"
+                        />
+                      )}
+                    </Col>
+                  </Row>
+                </CardBody>
+              )}
             </Form>
           </Card>
         </Col>
