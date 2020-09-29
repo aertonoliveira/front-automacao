449ea7e49a268e16e3b9c361ed10da81a8bd954b
Aline Alves <alinealves@MacBook-Pro-de-Aline.local>
2020-09-28 23:17:49 -0300

    ajuste do root Saga

--- src/store/rootSaga.js
+++ src/store/rootSaga.js
@@ -4,10 +4,10 @@ import user from './modules/user/saga';
 import cliente from './modules/clientes/saga';
 import produto from './modules/produtos/saga';
 import contaBancaria from './modules/contaBancaria/saga';
-export default function* rootSaga() {
-  return yield all([auth, user, cliente, produto]);
+
+
 
 
 export default function* rootSaga() {
-  return yield all([auth, user, cliente, contaBancaria]);
+  return yield all([auth, user, cliente, produto, contaBancaria]);
 }
